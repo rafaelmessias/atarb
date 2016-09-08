d44e617fc5797955e276eb73c234a5ae60b21f51
Rafael Martins <rmmartins@gmail.com>
2016-09-08 20:10:21 -0300

    I did not fix Debug mode

--- instructions.rb
+++ instructions.rb
@@ -125,7 +125,7 @@ end
 # ORA (Immediate)
 $instructions[0x09] = Proc.new do
   param = fetch()
-  puts "ORA \#$%02X" % param
+  puts "ORA \#$%02X" % param if Debug
   op_ORA(param)
   2
 end
@@ -133,7 +133,7 @@ end
 # ORA [Zero Page]
 $instructions[0x05] = Proc.new do
   addr = fetch()
-  puts "ORA $%02X" % addr
+  puts "ORA $%02X" % addr if Debug
   op_ORA($memory[addr])
   3
 end
@@ -141,7 +141,7 @@ end
 # BPL
 $instructions[0x10] = Proc.new do
   addr = get_relative_addr(fetch())
-  puts "BPL $%04X" % addr
+  puts "BPL $%04X" % addr if Debug
   cycles = 2
   if !$flag_N
     $reg_PC = addr
@@ -153,7 +153,7 @@ end
 
 # CLC
 $instructions[0x18] = Proc.new do
-  puts "CLC"
+  puts "CLC" if Debug
   $flag_C = false
   2
 end
@@ -161,7 +161,7 @@ end
 # JSR
 $instructions[0x20] = Proc.new do
   addr = get_absolute_addr(fetch(), fetch())
-  puts "JSR $%04X" % addr
+  puts "JSR $%04X" % addr if Debug
   stack_push(($reg_PC-1) >> 8)
   stack_push(($reg_PC-1) & 255)
   $reg_PC = addr
@@ -171,7 +171,7 @@ end
 # BIT [Zero Page]
 $instructions[0x24] = Proc.new do
   param = fetch()
-  puts "BIT $%02X" % param
+  puts "BIT $%02X" % param if Debug
   aux = $memory[param] & $reg_A
   $flag_N = $memory[param] > 127
   # TODO: implement flag_V
@@ -182,7 +182,7 @@ end
 # AND (Zero Page)
 $instructions[0x25] = Proc.new do
   param = fetch()
-  puts "AND $%02X" % param
+  puts "AND $%02X" % param if Debug
   op_AND($memory[param])
   3
 end
@@ -190,14 +190,14 @@ end
 # AND (Immediate)
 $instructions[0x29] = Proc.new do
   param = fetch()
-  puts "AND \#$%02X" % param
+  puts "AND \#$%02X" % param if Debug
   op_AND(param)
   2
 end
 
 # SEC
 $instructions[0x38] = Proc.new do
-  puts "SEC"
+  puts "SEC" if Debug
   $flag_C = true
   2
 end
@@ -205,7 +205,7 @@ end
 # EOR (Zero Page)
 $instructions[0x45] = Proc.new do
   param = fetch()
-  puts "EOR $%02X" % param
+  puts "EOR $%02X" % param if Debug
   op_EOR($memory[param])
   3
 end
@@ -213,21 +213,21 @@ end
 # LSR (Zero Page)
 $instructions[0x46] = Proc.new do
   addr = fetch()
-  puts "LSR $%02X" % addr
+  puts "LSR $%02X" % addr if Debug
   $memory[addr] = op_LSR($memory[addr])
   5
 end
 
 # PHA
 $instructions[0x48] = Proc.new do
-  puts "PHA"
+  puts "PHA" if Debug
   stack_push($reg_A)
   3
 end
 
 # PLA
 $instructions[0x68] = Proc.new do
-  puts "PLA"
+  puts "PLA" if Debug
   $reg_A = stack_pull()
   update_NZ_flags($reg_A)
   4
@@ -236,35 +236,35 @@ end
 # EOR (Immediate)
 $instructions[0x49] = Proc.new do
   param = fetch()
-  puts "EOR \#$%02X" % param
+  puts "EOR \#$%02X" % param if Debug
   op_EOR(param)
   2
 end
 
 # LSR [Implicit]
 $instructions[0x4A] = Proc.new do
-  puts "LSR A"
+  puts "LSR A" if Debug
   $reg_A = op_LSR($reg_A)
   2
 end
 
 # CLI
 $instructions[0x58] = Proc.new do
-  puts "CLI"
+  puts "CLI" if Debug
   $flag_Int = false
   2
 end
 
 # RTS
 $instructions[0x60] = Proc.new do
-  puts "RTS"
+  puts "RTS" if Debug
   $reg_PC = stack_pull() + stack_pull() * 256 + 1
   6
 end
 
 # SEI
 $instructions[0x78] = Proc.new do
-  puts "SEI"
+  puts "SEI" if Debug
   $flag_Int = true
   2
 end
@@ -272,7 +272,7 @@ end
 # STY (Zero Page)
 $instructions[0x84] = Proc.new do
   param = fetch()
-  puts "STY $%02X" % param
+  puts "STY $%02X" % param if Debug
   $memory[param] = $reg_Y
   3
 end
@@ -280,7 +280,7 @@ end
 # STA [Zero Page]
 $instructions[0x85] = Proc.new do
   param = fetch()
-  puts "STA $%02X" % param
+  puts "STA $%02X" % param if Debug
   $memory[param] = $reg_A
   3
 end
@@ -288,7 +288,7 @@ end
 # STA [Absolute]
 $instructions[0x8D] = Proc.new do
   addr = get_absolute_addr(fetch(), fetch())
-  puts "STA $%04X" % addr
+  puts "STA $%04X" % addr if Debug
   $memory[addr] = $reg_A
   4
 end
@@ -296,21 +296,21 @@ end
 # STX [Zero Page]
 $instructions[0x86] = Proc.new do
   param = fetch()
-  puts "STX $%02X" % param
+  puts "STX $%02X" % param if Debug
   $memory[param] = $reg_X
   3
 end
 
 # DEY
 $instructions[0x88] = Proc.new do
-  puts "DEY"
+  puts "DEY" if Debug
   $reg_Y = op_DEC($reg_Y)
   2
 end
 
 # TXA
 $instructions[0x8A] = Proc.new do
-  puts "TXA"
+  puts "TXA" if Debug
   $reg_A = $reg_X
   update_NZ_flags($reg_A)
   2
@@ -319,7 +319,7 @@ end
 # BCC
 $instructions[0x90] = Proc.new do
   addr = get_relative_addr(fetch())
-  puts "BCC $%04X" % addr
+  puts "BCC $%04X" % addr if Debug
   cycles = 2
   if !$flag_C
     $reg_PC = addr
@@ -332,7 +332,7 @@ end
 # STY (Zero Page,X)
 $instructions[0x94] = Proc.new do
   param = fetch()
-  puts "STY \$%02X,X" % param
+  puts "STY \$%02X,X" % param if Debug
   $memory[byte_add(param, $reg_X)] = $reg_Y
   4
 end
@@ -340,14 +340,14 @@ end
 # STA (Zero Page,X)
 $instructions[0x95] = Proc.new do
   param = fetch()
-  puts "STA $%02X,X" % param
+  puts "STA $%02X,X" % param if Debug
   $memory[byte_add(param, $reg_X)] = $reg_A
   4
 end
 
 # TYA
 $instructions[0x98] = Proc.new do
-  puts "TYA"
+  puts "TYA" if Debug
   $reg_A = $reg_Y
   update_NZ_flags($reg_A)
   2
@@ -355,7 +355,7 @@ end
 
 # TXS
 $instructions[0x9A] = Proc.new do
-  puts "TXS"
+  puts "TXS" if Debug
   $reg_SP = $reg_X
   2
 end
@@ -363,7 +363,7 @@ end
 # LDY [Immediate]
 $instructions[0xA0] = Proc.new do
   param = fetch()
-  puts "LDY \#$%02X" % param
+  puts "LDY \#$%02X" % param if Debug
   $reg_Y = param
   update_NZ_flags($reg_Y)
   2
@@ -372,7 +372,7 @@ end
 # LDY [Absolute,X]
 $instructions[0xBC] = Proc.new do
   addr = get_absolute_addr(fetch(), fetch())
-  puts "LDY $%04X,X" % addr
+  puts "LDY $%04X,X" % addr if Debug
   $reg_Y = $memory[addr]
   update_NZ_flags($reg_Y)
   # TODO +1 if page crossed
@@ -382,7 +382,7 @@ end
 # LDX (Immediate)
 $instructions[0xA2] = Proc.new do
   param = fetch()
-  puts "LDX \#\$%02X" % param
+  puts "LDX \#\$%02X" % param if Debug
   $reg_X = param
   update_NZ_flags($reg_X)
   2
@@ -391,7 +391,7 @@ end
 # LDY (Zero Page)
 $instructions[0xA4] = Proc.new do
   param = fetch()
-  puts "LDY $%02X" % param
+  puts "LDY $%02X" % param if Debug
   $reg_Y = $memory[param]
   update_NZ_flags($reg_Y)
   3
@@ -400,7 +400,7 @@ end
 # LDA (Zero Page)
 $instructions[0xA5] = Proc.new do
   param = fetch()
-  puts "LDA \$%02X" % param
+  puts "LDA \$%02X" % param if Debug
   op_LDA($memory[param])
   3
 end
@@ -408,7 +408,7 @@ end
 # LDX [Zero Page]
 $instructions[0xA6] = Proc.new do
   param = fetch()
-  puts "LDX $%02X" % param
+  puts "LDX $%02X" % param if Debug
   $reg_X = $memory[param]
   update_NZ_flags($reg_X)
   3
@@ -416,7 +416,7 @@ end
 
 # TAY
 $instructions[0xA8] = Proc.new do
-  puts "TAY"
+  puts "TAY" if Debug
   $reg_Y = $reg_A
   update_NZ_flags($reg_Y)
   2
@@ -425,14 +425,14 @@ end
 # LDA (Immediate)
 $instructions[0xA9] = Proc.new do
   param = fetch()
-  puts "LDA \#\$%02X" % param
+  puts "LDA \#\$%02X" % param if Debug
   op_LDA(param)
   2
 end
 
 # TAX
 $instructions[0xAA] = Proc.new do
-  puts "TAX"
+  puts "TAX" if Debug
   $reg_X = $reg_A
   update_NZ_flags($reg_X)
   2
@@ -441,7 +441,7 @@ end
 # LDA (Absolute)
 $instructions[0xAD] = Proc.new do
   addr = get_absolute_addr(fetch(), fetch())
-  puts "LDA $%04X" % addr
+  puts "LDA $%04X" % addr if Debug
   op_LDA($memory[addr])
   4
 end
@@ -449,7 +449,7 @@ end
 # BCS
 $instructions[0xB0] = Proc.new do
   addr = get_relative_addr(fetch())
-  puts "BCS $%04X" % addr
+  puts "BCS $%04X" % addr if Debug
   cycles = 2
   if $flag_C
     $reg_PC = addr
@@ -462,7 +462,7 @@ end
 # LDA (Indirect), Y
 $instructions[0xB1] = Proc.new do
   addr = fetch()
-  puts "LDA ($%02X),Y" % addr
+  puts "LDA ($%02X),Y" % addr if Debug
   addr = get_indirect_y(addr)
   op_LDA($memory[addr])
   # TODO +1 if page crossed
@@ -472,7 +472,7 @@ end
 # LDY (Zero Page,X)
 $instructions[0xB4] = Proc.new do
   addr = fetch()
-  puts "LDY $%02X,X" % addr
+  puts "LDY $%02X,X" % addr if Debug
   $reg_Y = $memory[byte_add(addr, $reg_X)]
   update_NZ_flags($reg_Y)
   4
@@ -481,7 +481,7 @@ end
 # LDA (Zero Page,X)
 $instructions[0xB5] = Proc.new do
   addr = fetch()
-  puts "LDA $%02X,X" % addr
+  puts "LDA $%02X,X" % addr if Debug
   op_LDA($memory[byte_add(addr, $reg_X)])
   4
 end
@@ -489,7 +489,7 @@ end
 # LDA (Absolute, Y)
 $instructions[0xB9] = Proc.new do
   addr = get_absolute_addr(fetch(), fetch())
-  puts "LDA $%04X,Y" % addr
+  puts "LDA $%04X,Y" % addr if Debug
   # Should wrap the add to 16-bit
   op_LDA($memory[addr + $reg_Y])
   # TODO +1 if page crossed
@@ -499,7 +499,7 @@ end
 # LDA (Absolute,X)
 $instructions[0xBD] = Proc.new do
   addr = get_absolute_addr(fetch(), fetch())
-  puts "LDA $%04X,X" % addr
+  puts "LDA $%04X,X" % addr if Debug
   # Should wrap the add to 16-bit
   op_LDA($memory[addr + $reg_X])
   # TODO +1 if page crossed
@@ -509,7 +509,7 @@ end
 # LDX (Absolute, Y)
 $instructions[0xBE] = Proc.new do
   addr = get_absolute_addr(fetch(), fetch())
-  puts "LDX $%04X,Y" % addr
+  puts "LDX $%04X,Y" % addr if Debug
   # Should wrap the add to 16-bit
   $reg_X = $memory[addr + $reg_Y]
   update_NZ_flags($reg_X)
@@ -519,7 +519,7 @@ end
 
 # DEX
 $instructions[0xCA] = Proc.new do
-  puts "DEX"
+  puts "DEX" if Debug
   $reg_X = op_DEC($reg_X)
   2
 end
@@ -527,14 +527,14 @@ end
 # DEC [Zero Page]
 $instructions[0xC6] = Proc.new do
   addr = fetch()
-  puts "DEC $%02X" % addr
+  puts "DEC $%02X" % addr if Debug
   $memory[addr] = op_DEC($memory[addr])
   5
 end
 
 # INY
 $instructions[0xC8] = Proc.new do
-  puts "INY"
+  puts "INY" if Debug
   $reg_Y = byte_add($reg_Y, 1)
   update_NZ_flags($reg_Y)
   2
@@ -543,7 +543,7 @@ end
 # CMP [Immediate]
 $instructions[0xC9] = Proc.new do
   param = fetch()
-  puts "CMP \#$%02X" % param
+  puts "CMP \#$%02X" % param if Debug
   op_CMP($reg_A, param)
   2
 end
@@ -551,7 +551,7 @@ end
 # CMP [Zero Page]
 $instructions[0xC5] = Proc.new do
   addr = fetch()
-  puts "CMP $%02X" % addr
+  puts "CMP $%02X" % addr if Debug
   op_CMP($reg_A, $memory[addr])
   3
 end
@@ -559,7 +559,7 @@ end
 # BNE
 $instructions[0xD0] = Proc.new do
   addr = get_relative_addr(fetch())
-  puts "BNE $%04X" % addr
+  puts "BNE $%04X" % addr if Debug
   cycles = 2
   if !$flag_Z
     $reg_PC = addr
@@ -572,7 +572,7 @@ end
 # BMI
 $instructions[0x30] = Proc.new do
   addr = get_relative_addr(fetch())
-  puts "BMI $%04X" % addr
+  puts "BMI $%04X" % addr if Debug
   cycles = 2
   if $flag_N
     $reg_PC = addr
@@ -584,7 +584,7 @@ end
 
 # CLD
 $instructions[0xD8] = Proc.new do
-  puts "CLD"
+  puts "CLD" if Debug
   $flag_D = false
   2
 end
@@ -592,7 +592,7 @@ end
 # CPX [Immediate]
 $instructions[0xE0] = Proc.new do
   param = fetch()
-  puts "CPX \#$%02X" % param
+  puts "CPX \#$%02X" % param if Debug
   op_CMP($reg_X, param)
   2
 end
@@ -600,7 +600,7 @@ end
 # CPX [Zero Page]
 $instructions[0xE4] = Proc.new do
   addr = fetch()
-  puts "CPX $%02X" % addr
+  puts "CPX $%02X" % addr if Debug
   op_CMP($reg_X, $memory[addr])
   3
 end
@@ -608,7 +608,7 @@ end
 # CPY [Immediate]
 $instructions[0xC0] = Proc.new do
   param = fetch()
-  puts "CPY \#$%02X" % param
+  puts "CPY \#$%02X" % param if Debug
   op_CMP($reg_Y, param)
   2
 end
@@ -616,7 +616,7 @@ end
 # INC (Zero Page)
 $instructions[0xE6] = Proc.new do
   param = fetch()
-  puts "INC $%02X" % param
+  puts "INC $%02X" % param if Debug
   $memory[param] = byte_add($memory[param], 1)
   update_NZ_flags($memory[param])
   5
@@ -624,7 +624,7 @@ end
 
 # INX
 $instructions[0xE8] = Proc.new do
-  puts "INX"
+  puts "INX" if Debug
   $reg_X = byte_add($reg_X, 1)
   update_NZ_flags($reg_X)
   2
@@ -633,7 +633,7 @@ end
 # SBC [Immediate]
 $instructions[0xE9] = Proc.new do
   param = fetch()
-  puts "SBC \#$%02X" % param
+  puts "SBC \#$%02X" % param if Debug
   op_SBC(param)
   2
 end
@@ -641,7 +641,7 @@ end
 # SBC [Zero Page]
 $instructions[0xE5] = Proc.new do
   addr = fetch()
-  puts "SBC $%02X" % addr
+  puts "SBC $%02X" % addr if Debug
   op_SBC($memory[addr])
   3
 end
@@ -649,7 +649,7 @@ end
 # BEQ
 $instructions[0xF0] = Proc.new do
   addr = get_relative_addr(fetch())
-  puts "BEQ $%04X" % addr
+  puts "BEQ $%04X" % addr if Debug
   cycles = 2
   if $flag_Z
     $reg_PC = addr
@@ -661,21 +661,21 @@ end
 
 # SED
 $instructions[0xF8] = Proc.new do
-  puts "SED"
+  puts "SED" if Debug
   $flag_D = true
   2
 end
 
 # NOP
 $instructions[0xEA] = Proc.new do
-  puts "NOP"
+  puts "NOP" if Debug
   2
 end
 
 # JMP [Absolute]
 $instructions[0x4C] = Proc.new do
   addr = get_absolute_addr(fetch(), fetch())
-  puts "JMP $%04X" % addr
+  puts "JMP $%04X" % addr if Debug
   $reg_PC = addr
   3
 end
@@ -683,14 +683,14 @@ end
 # ROL [Zero Page]
 $instructions[0x26] = Proc.new do
   addr = fetch()
-  puts "ROL $%02X" % addr
+  puts "ROL $%02X" % addr if Debug
   $memory[addr] = op_ROL($memory[addr])
   5
 end
 
 # ROL
 $instructions[0x2A] = Proc.new do
-  puts "ROL"
+  puts "ROL" if Debug
   $reg_A = op_ROL($reg_A)
   2
 end
